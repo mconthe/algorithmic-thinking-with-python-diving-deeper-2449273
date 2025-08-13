d9f04165aef9694bfdce1e04ef3e13f8ba26f9a3
mconthe <96492581+mconthe@users.noreply.github.com>
2025-08-13 08:45:37 +0000

    fibonacci

--- 02_06/fibonacci.py
+++ 02_06/fibonacci.py
@@ -1,5 +1,14 @@
 def fibonacci_recursive(n):
-    pass
+    
+    if n == 0 :
+        return 0
+
+    elif n == 1:
+        return 1
+
+    else:
+        return fibonacci_recursive(n -1) + fibonacci_recursive(n-2)  
+
 
 
 for i in range(8):
